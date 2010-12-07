36b5ad6b1338fd3c0ead08e8341dd1242c8f2ccc
duongvuxuan@gmail.com <duongvuxuan@gmail.com@d2ad7803-4af0-275d-adb2-c38369db1708>
2010-12-07 03:12:50 +0000

    server.c

--- netpro/server_lib.h
+++ netpro/server_lib.h
@@ -5,6 +5,20 @@
  *  Author: PREDATOR TEAM
  */
 #include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/select.h>
+
+#ifndef MAX_SOCKNUM
+# define MAX_SOCKNUM FD_SETSIZE
+
+
+#endif
 
 #ifndef SERVER_LIB_H_
 #define SERVER_LIB_H_
@@ -12,7 +26,7 @@
 
 #endif /* SERVER_LIB_H_ */
 
-int create_server_socket(int port_number);
+int create_server_socket(char *port_number);
 int get_command_result(char *result, char *command);
 int send_command_result(char *result, int i32ConnectFD);
 char recieve_command_character(int i32ConnectFD);
