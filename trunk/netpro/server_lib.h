e2a3bdc2a8f20e3c40456d20f259b04d62e53d0f
duongvuxuan@gmail.com <duongvuxuan@gmail.com@d2ad7803-4af0-275d-adb2-c38369db1708>
2010-12-07 02:27:44 +0000

    Add Program API!!

--- trunk/netpro/server_lib.h
+++ trunk/netpro/server_lib.h
@@ -4,9 +4,16 @@
  *  Created on: Dec 3, 2010
  *  Author: PREDATOR TEAM
  */
+#include <stdio.h>
 
 #ifndef SERVER_LIB_H_
 #define SERVER_LIB_H_
 
 
 #endif /* SERVER_LIB_H_ */
+
+int create_server_socket(int port_number);
+int get_command_result(char *result, char *command);
+int send_command_result(char *result, int i32ConnectFD);
+char recieve_command_character(int i32ConnectFD);
+
