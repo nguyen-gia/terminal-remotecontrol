e7a8037f7f93b18851b5798ea2aead91d38830a2
duongvuxuan@gmail.com <duongvuxuan@gmail.com@d2ad7803-4af0-275d-adb2-c38369db1708>
2010-12-07 02:27:44 +0000

    Add Program API!!

--- netpro/client_lib.h
+++ netpro/client_lib.h
@@ -4,9 +4,13 @@
  *  Created on: Dec 3, 2010
  *  Author: PREDATOR TEAM
  */
-
+#include <stdio.h>
 #ifndef CLIENT_H_
 #define CLIENT_H_
 
 
 #endif /* CLIENT_H_ */
+
+int create_client_socket(int port_number, char *server_ip_address);
+int send_command_character(int ch, int i32ConnectFD);
+int recieve_command_result(char *result, int i32ConnectFD);
